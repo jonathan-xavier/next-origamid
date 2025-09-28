bce899cfde6f0a005c7cad973a3e022a816a888d
jonathan xavier rodrigues <jonathan@MacBook-Air-de-jonathan.local>
2025-09-28 15:40:23 -0300

    criando imc calculo

--- src/app/imc/page.tsx
+++ src/app/imc/page.tsx
@@ -1,8 +1,38 @@
+'use client'
+import { useState } from "react"
+
 export default function ImcPage() {
+    const [height, setHeigh] = useState<number>()
+    const [weight, setWeight] = useState<number>()
+    const [result, setResult] = useState<number>()
+
+
+    const calculateImc = () => {
+        if(!height && !weight){
+            return null
+        }
+
+        if (height) {
+            const base = height * height;
+            const total = (weight ?? 0) / base
+            const [refinedValue] = total.toString().split('.')
+            
+            setResult(Number(refinedValue))
+        }
+    }
     return (
-        <div>
+        <main>
             <h2>Calcular Imc</h2>
-            <ImcPage/>
-        </div>
+            <input type="number" placeholder="altura"
+                onChange={(e) => setHeigh(Number(e.target.value))} />
+
+            <input type="number" placeholder="peso"
+                onChange={(e) => setWeight(Number(e.target.value))} />
+
+            <button onClick={calculateImc}>calcular</button>
+
+            <span>Resultado IMC: {result}</span>
+
+        </main>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default async function Home() {
   
   return (
       <main>
-        <h1>Home</h1> 
+        <h1>Homesdss</h1> 
         {/* <Acesso/>        */}
       </main>
   );
