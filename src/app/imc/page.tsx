1b1312175be06a5d7149e6dc0bf09bc1823dc37f
jonathan xavier rodrigues <jonathan@MacBook-Air-de-jonathan.local>
2025-09-28 16:12:25 -0300

    ajustando codigo de acordo com o desafio

--- src/app/imc/page.tsx
+++ src/app/imc/page.tsx
@@ -1,38 +1,12 @@
-'use client'
-import { useState } from "react"
+
+import ImcCalc from "@/components/imc-calc"
 
 export default function ImcPage() {
-    const [height, setHeigh] = useState<number>()
-    const [weight, setWeight] = useState<number>()
-    const [result, setResult] = useState<number>()
-
-
-    const calculateImc = () => {
-        if(!height && !weight){
-            return null
-        }
-
-        if (height) {
-            const base = height * height;
-            const total = (weight ?? 0) / base
-            const [refinedValue] = total.toString().split('.')
-            
-            setResult(Number(refinedValue))
-        }
-    }
     return (
         <main>
-            <h2>Calcular Imc</h2>
-            <input type="number" placeholder="altura"
-                onChange={(e) => setHeigh(Number(e.target.value))} />
-
-            <input type="number" placeholder="peso"
-                onChange={(e) => setWeight(Number(e.target.value))} />
-
-            <button onClick={calculateImc}>calcular</button>
-
-            <span>Resultado IMC: {result}</span>
-
+            <h1>Imc calculo</h1>
+            <ImcCalc/>
         </main>
     )
+    
 }
--- src/components/imc-calc.tsx
+++ src/components/imc-calc.tsx
@@ -1,7 +1,40 @@
+"use client"
+
+
+import { useState } from "react"
 export default function ImcCalc() {
+
+    const [height, setHeigh] = useState<number>()
+    const [weight, setWeight] = useState<number>()
+    const [result, setResult] = useState<number>()
+
+
+    const calculateImc = () => {
+        if(!height && !weight){
+            return null
+        }
+
+        if (height) {
+            const base = height * height;
+            const total = (weight ?? 0) / base
+
+            setResult(Number(total.toFixed(2)))
+        }
+    }
+
     return (
-        <div>
-            <h2>imc</h2>
-        </div>
+        <main>
+            <h2>Calcular Imc</h2>
+            <input type="number" placeholder="altura"
+                onChange={(e) => setHeigh(Number(e.target.value))} />
+
+            <input type="number" placeholder="peso"
+                onChange={(e) => setWeight(Number(e.target.value))} />
+
+            <button onClick={calculateImc}>calcular</button>
+
+            <span>Resultado IMC: {result}</span>
+
+        </main>
     )
 }
